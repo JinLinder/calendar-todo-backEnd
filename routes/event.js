d8d7e8b61b1eeeb991e8a0ed65bf8cf983d98056
Jin Linder <jinlinder@hotmail.com>
2023-03-20 14:49:52 +0100

    add readme file

--- routes/event.js
+++ routes/event.js
@@ -23,12 +23,13 @@ router.post('/add', function (req, res, next) {
             end,
             id
     })
+    console.log(req.body)
     req.body.save()
     res.send("event saved")
 })
 
 /*Delete events*/
-router.delete('/delete/:id', function(req, res){
+router.delete('/delete/:id', function(req, res, next){
     event.findByIdAndRemove(req.params.id) 
     .then(doc => {
         if(!doc){ return res.status(404).end();}
